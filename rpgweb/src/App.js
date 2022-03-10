97616a33b7cd0da53a56f89d7b4f9e54cf982c36
Faraaz <faraazmohsin100@gmail.com>
2022-03-09 21:57:58 -0500

    added routing for pages

--- rpgweb/src/App.js
+++ rpgweb/src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import { StudentHome } from './containers/student/studenthome';
+import { StudentAbout } from './containers/student/studentabout';
+import { StudentBlog } from './containers/student/studentblog';
+import { StudentEvents } from './containers/student/studentevents';
+import { StudentTeam } from './containers/student/studentteam';
+import { StudentContact } from './containers/student/studentcontact';
+import { Landing } from './containers/landing/landing';
+import { Rpg } from './containers/rpg/rpg';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+
+            {/* Routes for landing and rpg split pages */}
+            <Route path='/' element={<Landing/>}/>
+            <Route path='/rpg' element={<Rpg/>}/>
+
+            {/* Routes for student components */}
+            <Route path='/studenthome' element={<StudentHome />}/>
+            <Route path='/studentabout' element={<StudentAbout />}/>
+            <Route path='/studentblog' element={<StudentBlog />}/>
+            <Route path='/studentevents' element={<StudentEvents />}/>
+            <Route path='/studentteam' element={<StudentTeam />}/>
+            <Route path='/studentcontact' element={<StudentContact />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- rpgweb/src/containers/landing/landing.jsx
+++ rpgweb/src/containers/landing/landing.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LandingContainer = styled.div`
-
+    display: flex;
+    position: relative;
+    
 `;
 
 export function Landing() {
