c65e4d33e221dad4f6dcd891db29bd0e6b3968aa
arriiibb <arib.kamal@ryerson.ca>
2022-03-12 13:53:41 -0500

    Added background image file

--- rpgweb/src/containers/student/studentteam.jsx
+++ rpgweb/src/containers/student/studentteam.jsx
@@ -5,11 +5,32 @@ const StudentTeamContainer = styled.div`
 
 `;
 
+const Background = styled.div`
+`
+const Title1 = styled.div`
+    display: flex;
+    justify-content: center;
+    color: black;
+`
+
+const Title2 = styled.div`
+    display: flex;
+    justify-content: center;
+    color: #F3BD11;
+`
+
 {/* Render components */}
 export function StudentTeam() {
+
+
     return (
         <StudentTeamContainer>
-            
+            <Title1>
+                A Curated Team
+            </Title1>
+            <Title2>
+                Paving your Way into space
+            </Title2>
         </StudentTeamContainer>
     )
 };
