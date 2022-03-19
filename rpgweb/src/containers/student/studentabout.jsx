b51baf663a10739ec052a17223dea2433d782fa4
Alex <alex.gyumushyan@gmail.com>
2022-03-19 18:05:20 -0400

    working on student about page

--- rpgweb/src/containers/student/studentabout.jsx
+++ rpgweb/src/containers/student/studentabout.jsx
@@ -8,11 +8,64 @@ const StudentAboutContainer = styled.div`
     background-image: url(${BackPic});
 `;
 
+const OurMilestones = styled.div`
+    position: absolute;
+    top: 15vh;
+    left: 15vw;
+    
+    background-image: -webkit-linear-gradient(#57FFDC, #F3BD11);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    font-family: poppins;
+    font-weight: bold;
+    font-size: 3.6em;
+`;
+
+const WeStriveToLead = styled.div`
+    position: absolute;
+    top: 50vh;
+    left: 30vw;
+    
+    background-image: -webkit-linear-gradient(#57FFDC, #F3BD11);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    font-family: poppins;
+    font-weight: bold;
+    font-size: 3.6em;
+`;
+
+const Heading1 = styled.div`
+    font-family: poppins;
+    color: white;
+    white-space: pre-line;
+
+    font-size: 25px;
+    margin-left: 15%;
+    margin-top: 10%;
+`;
+
 {/* Render components */}
 export function StudentAbout() {
     return (
         <StudentAboutContainer>
             <StudentMenu/>
+
+            <OurMilestones>
+                Our Milestones
+            </OurMilestones>
+
+
+            <Heading1>
+                At RPG We've established {"\n"}
+                A Community Of Driven {"\n"}
+                Students
+            </Heading1>
+
+            <WeStriveToLead>
+                We Strive To Lead Innovation
+            </WeStriveToLead>
             
         </StudentAboutContainer>
     )
