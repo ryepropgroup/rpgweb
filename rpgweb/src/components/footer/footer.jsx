3c0692731f48d072bfebce149b6cbbe2ebffe3e4
Alex <alex.gyumushyan@gmail.com>
2022-03-26 23:19:34 -0400

    started working on footer

---
+++ rpgweb/src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+
+
+export function Footer() {
+
+    return (
+        <p1>
+            add stuff here
+        </p1>
+
+    )
+};
